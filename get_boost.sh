175d64687f95783d6c0a2465f2a51aa3d2656bdb
wdidada <wdidada@qq.com>
2023-12-25 11:43:47 +0800

    add boost

--- get_boost.sh
+++ get_boost.sh
@@ -2,6 +2,7 @@
 #git clone https://github.com/boostorg/boost.git
 wget https://boostorg.jfrog.io/artifactory/main/release/1.65.0/source/boost_1_65_0.tar.gz
 #cd boost/
+tar -zxvf boost_1_65_0.tar.gz
 cd boost_*/
 #git checkout boost-1.65.0
 git submodule init
