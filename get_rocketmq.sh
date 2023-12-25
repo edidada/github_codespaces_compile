6575293d195e6243c0397a153f9dfb368513deea
wdidada <wdidada@qq.com>
2023-12-25 18:34:16 +0800

    modify rq script

--- get_rocketmq.sh
+++ get_rocketmq.sh
@@ -2,8 +2,8 @@
 git clone https://github.com/apache/rocketmq.git
 cd rocketmq
 git checkout rocketmq-all-4.3.0
-mvn clean package
+#mvn clean package
 mvn -B clean apache-rat:check
 mvn -B package jacoco:report coveralls:report
 mvn clean install -Pit-test
-mvn sonar:sonar -Psonar-apache
+#mvn sonar:sonar -Psonar-apache
