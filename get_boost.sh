cca4d2441dd212c6df19abe08e1cc4557c1d3c7a
wdidada <wdidada@qq.com>
2023-12-26 08:53:25 +0800

    add boost

--- get_boost.sh
+++ get_boost.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #git clone https://github.com/boostorg/boost.git
+echo "download boost_1_65_0"
 wget https://boostorg.jfrog.io/artifactory/main/release/1.65.0/source/boost_1_65_0.tar.gz
 #cd boost/
 tar -zxvf boost_1_65_0.tar.gz
 cd boost_*/
 #git checkout boost-1.65.0
+echo "git submodule"
 git submodule init
 git submodule update
+echo "run bootstrap script"
 ./bootstrap.sh --with-libraries=all
 sudo ./b2 install
 ./b2 --show-libraries
