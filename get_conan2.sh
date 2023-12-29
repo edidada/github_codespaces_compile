2c5dee8134cc68f837419e4689462ba4747a93d4
WuCheng <xxx@qq.com>
2023-12-29 11:15:40 +0800

    add modify

--- get_conan2.sh
+++ get_conan2.sh
@@ -3,7 +3,7 @@
 pip3 install conan
 sudo update
 sudo apt-get install autoconf automake libtool curl make unzip -y
-git clone https://github.com/conan-io/examples.git
+git clone https://github.com/conan-io/examples2.git
 cd examples2/tutorial/consuming_packages/tool_requires
 conan install . --output-folder=build --build=missing
 cd build
