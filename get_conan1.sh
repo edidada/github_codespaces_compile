7059ab2a78a42674415fcb1d217b0bd499905813
WuCheng <xxx@qq.com>
2023-12-29 14:20:28 +0800

    add modify

--- get_conan1.sh
+++ get_conan1.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-#! /bin/bash
-pip3 install conan==1.62.0
-conan profile update settings.compiler.libcxx=libstdc++11 default
 sudo update
 sudo apt-get install autoconf automake libtool curl make unzip -y
+pip3 install conan==1.62.0
+conan profile update settings.compiler.libcxx=libstdc++11 default
 git clone https://github.com/conan-io/examples.git
 cd examples/libraries/poco/md5
 rm -rf build
