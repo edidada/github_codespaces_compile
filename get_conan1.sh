d6369ecb6d2ba033dbe6db9724a21c60ac799397
WuCheng <xxx@qq.com>
2023-12-29 11:21:21 +0800

    add modify

--- get_conan1.sh
+++ get_conan1.sh
@@ -5,8 +5,9 @@ sudo update
 sudo apt-get install autoconf automake libtool curl make unzip -y
 git clone https://github.com/conan-io/examples.git
 cd examples/tutorial/consuming_packages/tool_requires
-conan install . --output-folder=build --build=missing
+rm -rf build
 cd build
+conan install .. --output-folder=build --build=missing
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 ./compressor
