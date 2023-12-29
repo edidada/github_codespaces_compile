cbd3c455cbf5ec02fcd0d2c241d6da5475d2cac3
WuCheng <xxx@qq.com>
2023-12-29 14:07:38 +0800

    add modify

--- get_conan1.sh
+++ get_conan1.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 #! /bin/bash
 pip3 install conan==1.62.0
+conan profile update settings.compiler.libcxx=libstdc++11 default
 sudo update
 sudo apt-get install autoconf automake libtool curl make unzip -y
 git clone https://github.com/conan-io/examples.git
 cd examples/tutorial/consuming_packages/tool_requires
 rm -rf build
 mkdir build
+conan install . --output-folder=build --build=missing
 cd build
-conan install .. --output-folder=build --build=missing
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 ./compressor
-cd examples/example/conanfile/package_info/components
-conan create .
