204c94f03a5fb9bf466cfadead2fea5f72c8b6cf
WuCheng <xxx@qq.com>
2023-12-29 11:11:22 +0800

    add conan1.yml

---
+++ get_conan1.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#! /bin/bash
+pip3 install conan==1.62.0
+sudo update
+sudo apt-get install autoconf automake libtool curl make unzip -y
+git clone https://github.com/conan-io/examples.git
+cd examples/tutorial/consuming_packages/tool_requires
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+./compressor
+cd examples2/example/conanfile/package_info/components
+conan create .
