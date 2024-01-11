6434ead5c2314c0d57189cc3d7bcae630e38a626
WuCheng <xxx@qq.com>
2024-01-11 19:10:04 +0800

    change curl

--- get_curl.sh
+++ get_curl.sh
@@ -4,8 +4,8 @@ sudo apt-get install autoconf automake m4 libtool -y
 git clone https://github.com/curl/curl.git
 cd curl
 git checkout curl-8_5_0
-autoconf
+autoconf --with-openssl
 chmod +x configure
-./configure
+./configure --with-openssl
 make
 sudo make install
