125b975848b0469d78a215f33fbf696ab47e7e02
WuCheng <xxx@qq.com>
2024-01-11 20:24:58 +0800

    modify firefox

--- get_firefox.sh
+++ get_firefox.sh
@@ -4,7 +4,7 @@ sudo apt-get install curl python3 python3-pip -y
 python3 -m pip install --user mercurial
 echo 'export PATH="'"$(python3 -m site --user-base)"'/bin:$PATH"' >> ~/.bashrc
 curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py -O
-python3 bootstrap.py
+echo | python3 bootstrap.py
 cd mozilla-unified
 hg up -C central
 ./mach build
