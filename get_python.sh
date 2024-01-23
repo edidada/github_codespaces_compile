4e27664874c8c6a7bfb5b9c75bcf8ae39f00d631
wdidada <xxx@qq.com>
2024-01-23 17:43:06 +0800

    add python2.7

---
+++ get_python.sh
@@ -0,0 +1,10 @@
+#! /usr/bash
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev -y
+wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+tar -xf Python-2.7.18.tgz
+cd Python-2.7.18
+./configure --enable-optimizations
+make -j 4
+sudo make altinstall
+python2.7 --version
