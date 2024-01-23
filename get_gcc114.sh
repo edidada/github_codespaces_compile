8302e9bfcff5c154c3d663601172f4b9bf220baa
wdidada <xxx@qq.com>
2024-01-23 18:50:08 +0800

    modify gcc

---
+++ get_gcc114.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libssl-dev libreadline-dev libsqlite3-dev wget llvm -y
+wget https://mirror.linux-ia64.org/gnu/gcc/releases/gcc-11.4.0/gcc-11.4.0.tar.gz -O gcc-11.4.0.tar.gz --no-check-certificate
+ls -la gcc-11.4.0.tar.gz
+tar xzvf gcc-11.4.0.tar.gz
+cd gcc-11.4.0
+./contrib/download_prerequisites
+./configure --enable-languages=c,c++ --enable-multilib --with-system-zlib --with-system-bzlib --with-system-readline --with-system-sqlite --with-system-ncurses --with-system-libffi --with-plugindir=/usr/lib64/gcc/plugin --with-pkgversion="Ubuntu 20" --with-bugurl="https://bugs.ubuntu.com" --enable-shared --enable-static --enable-threads=posix --enable-checking=release --enable-optimizations --enable-lto-plugin --enable-libstdcxx-pch --enable-default-pie --enable-default-ssp --enable-libssp --enable-libgomp --enable-libquadmath --enable-libmpx --enable-libunwind --enable-libsanitizer=address,undefined CC=gcc CXX=g++
+make all-gcc all-target-libgcc
+sudo make install-gcc install-target-libgcc
