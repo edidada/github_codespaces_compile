bacb8fb3aee9b85a9148a4299d3fd2f3b3f773ee
wdidada <xxx@qq.com>
2024-01-23 12:31:27 +0800

    add miniob

--- get_miniob.sh
+++ get_miniob.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+sudo apt update
+sudo apt install libevent-dev -y
+dpkg -L libevent-dev | grep LibeventConfig.cmake
 git clone https://github.com/oceanbase/miniob.git
 cd miniob
 bash build.sh init
