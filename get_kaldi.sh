0c6ae1715902ed6a2579966e2e1abb3824d3c69e
wdidada <xxx@qq.com>
2024-01-23 17:40:52 +0800

    add kaldi

--- get_kaldi.sh
+++ get_kaldi.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 git clone https://github.com/kaldi-asr/kaldi.git -b 5.4
 cd kaldi
-chmod +x ./INSTALL
-./INSTALL
+sudo apt update
+sudo apt-get install  sox subversion libatlas3-base -y
+#sudo apt-get install  g++ zlib1g-dev make automake autoconf bzip2 unzip wget sox libtool subversion python2.7 python3 libatlas-dev libatlas-base-dev -y
 cd tools && extras/check_dependencies.sh
-sudo apt-get install  g++ zlib1g-dev make automake autoconf bzip2 unzip wget sox libtool subversion python2.7 python3 libatlas-dev libatlas-base-dev -y
 make
 extras/install_irstlm.sh
