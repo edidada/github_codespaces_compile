af7eb65392da87104d38a16705392d6909a320af
wdidada <xxx@qq.com>
2024-01-23 12:42:48 +0800

    modify miniob

--- get_miniob.sh
+++ get_miniob.sh
@@ -2,7 +2,15 @@
 sudo apt update
 sudo apt install libevent-dev -y
 dpkg -L libevent-dev | grep LibeventConfig.cmake
+git clone https://github.com/libevent/libevent.git
+cd libevent
+git checkout release-2.1.12-stable
+mkdir build && cd build
+cmake ..
+make
+sudo make install
+cd ..
 git clone https://github.com/oceanbase/miniob.git
 cd miniob
-bash build.sh init
-bash build.sh release
+sudo bash build.sh init
+sudo bash build.sh release
