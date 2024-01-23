fe4c91d41e4a726df7eb039e45d2a2b7383557a4
wdidada <xxx@qq.com>
2024-01-23 18:42:46 +0800

    modify gcc

--- get_gcc.sh
+++ get_gcc.sh
@@ -6,6 +6,6 @@ ls -la gcc-11.2.0.tar.gz
 tar -zxvf gcc-11.2.0.tar.gz
 cd gcc-11.2.0
 ./contrib/download_prerequisites
-./configure --enable-languages=c,c++ --disable-multilib --with-system-zlib --with-system-bzlib --with-system-readline --with-system-sqlite --with-system-ncurses --with-system-libffi --with-plugindir=/usr/lib64/gcc/plugin --with-pkgversion="Ubuntu 20" --with-bugurl="https://bugs.ubuntu.com" --enable-shared --enable-static --enable-threads=posix --enable-checking=release --enable-optimizations --enable-lto-plugin --enable-libstdcxx-pch --enable-default-pie --enable-default-ssp --enable-libssp --enable-libgomp --enable-libquadmath --enable-libmpx --enable-libunwind --enable-libsanitizer=address,undefined CC=gcc CXX=g++
+./configure --enable-languages=c,c++ --enable-multilib --with-system-zlib --with-system-bzlib --with-system-readline --with-system-sqlite --with-system-ncurses --with-system-libffi --with-plugindir=/usr/lib64/gcc/plugin --with-pkgversion="Ubuntu 20" --with-bugurl="https://bugs.ubuntu.com" --enable-shared --enable-static --enable-threads=posix --enable-checking=release --enable-optimizations --enable-lto-plugin --enable-libstdcxx-pch --enable-default-pie --enable-default-ssp --enable-libssp --enable-libgomp --enable-libquadmath --enable-libmpx --enable-libunwind --enable-libsanitizer=address,undefined CC=gcc CXX=g++
 make all-gcc all-target-libgcc
 sudo make install-gcc install-target-libgcc
