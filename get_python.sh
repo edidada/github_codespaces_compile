91336bc1a42b289e632a68b6ff2d379aaa7266ab
Willis Barts <edidada555@gmail.com>
2024-01-23 21:55:43 +0800

    Update get_python.sh

--- get_python.sh
+++ get_python.sh
@@ -4,7 +4,7 @@ sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-
 wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
 tar -xf Python-2.7.18.tgz
 cd Python-2.7.18
-./configure --enable-optimizations
+./configure --enable-optimizations --without-tests
 make -j 4
 sudo make altinstall
 python2.7 --version
