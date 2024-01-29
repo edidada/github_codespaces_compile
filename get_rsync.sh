e83c6e4a967381cb38a19b80d54ead930db7b7db
wdidada <xxx@qq.com>
2024-01-29 19:56:34 +0800

    modify rsync

--- get_rsync.sh
+++ get_rsync.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 wget --no-check-certificate https://download.samba.org/pub/rsync/src/rsync-3.2.7.tar.gz -O rsync-3.2.7.tar.gz
+tar -zxvf rsync-3.2.7.tar.gz
+cd rsync-3.2.7
 sudo apt install -y gcc g++ gawk autoconf automake python3-cmarkgfm acl libacl1-dev attr libattr1-dev libxxhash-dev libzstd-dev liblz4-dev libssl-dev
 ./configure
 make
