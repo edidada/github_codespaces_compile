8e328fa5e3d47e6287c0c45fce53a7fbc594f3d2
wdidada <xxx@qq.com>
2024-02-06 20:47:17 +0800

    add llvm

---
+++ get_llvm.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+git checkout llvmorg-17.0.6
+mkdir build && cd build
+cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
+make
