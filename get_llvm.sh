a1c1164e65bc208e3eebb3a83a557030e22207fe
wdidada <xxx@qq.com>
2024-02-07 10:27:21 +0800

    modify llvm

--- get_llvm.sh
+++ get_llvm.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 sudo apt update
-git clone https://github.com/llvm/llvm-project.git
+git clone -b llvmorg-17.0.6 https://github.com/llvm/llvm-project.git
 cd llvm-project
-git checkout llvmorg-17.0.6
 mkdir build && cd build
 cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
 make -j5
