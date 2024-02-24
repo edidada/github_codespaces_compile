32baf368d0b6e371bc6d21c6888d6fc7d1c41a5d
edidada <sandisks555@gmail.com>
2024-02-24 03:52:43 +0000

    add

--- get_mysql8.sh
+++ get_mysql8.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 # sudo apt install libboost1.65-all-dev -y
 # sudo apt install libboost1.67-all-dev -y
-git clone -b boost-1.65.0 https://github.com/boostorg/boost.git
-cd boost/
-git submodule init
-git submodule update
-./bootstrap.sh --with-libraries=all --prefix=/usr/local/boost
-sudo ./b2 install
-cd ../
+wget http://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.tar.gz
 git clone -b mysql-8.0.4 https://github.com/mysql/mysql-server.git
 cd mysql-server
 rm -rf build
 mkdir build && cd build
 export BOOST_ROOT=/usr/local/boost  
 export BOOST_INCLUDEDIR=$BOOST_ROOT/include
-cmake .. -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../../boost -DWITH_SSL=system
+cmake .. -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../../boost_1_65_0.tar.gz -DWITH_SSL=system
 make -j5
