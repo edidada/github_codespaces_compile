9d716632bcbf7d0b9b708778b0641fcec74e04c8
wdidada <xxx@qq.com>
2024-02-28 20:38:49 +0800

    modify ffmpeg

--- get_ffmpeg.sh
+++ get_ffmpeg.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 cd "$(dirname "$0")"
+wget --no-check-certificate https://github.com/FFmpeg/FFmpeg/archive/refs/tags/n6.1.1.tar.gz -O n6.1.1.tar.gz
+tar -zxvf n6.1.1.tar.gz
+cd FFmpeg*
 mkdir -p ffmpeg-samples
-./configure --samples=ffmpeg-samples --cc=$CC
+./configure --samples=ffmpeg-samples --cc=$CC --disable-x86asm
 make -j 8
 make fate-rsync
 make check -j 8
