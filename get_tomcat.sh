c55d49ea04b667729f93b60bd7013f6d915aaab2
wdidada <xxx@qq.com>
2024-02-29 20:20:58 +0800

    modify tomcat

--- get_tomcat.sh
+++ get_tomcat.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 sudo apt-get update
-wget http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.15/src/apache-tomcat-8.5.15-src.zip -O apache-tomcat-8.5.15-src.zip
-unzip apache-tomcat-8.5.15-src.zip
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.99/src/apache-tomcat-8.5.99-src.zip -O apache-tomcat-8.5.99-src.zip
+unzip apache-tomcat-8.5.99-src.zip
 cd apache*
 ant ide-eclipse
