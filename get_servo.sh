64249c57548ee25666aa83445e9eb684a019e074
wdidada <xxx@qq.com>
2024-02-29 09:33:02 +0800

    modify servo

--- get_servo.sh
+++ get_servo.sh
@@ -3,4 +3,5 @@ cd "$(dirname "$0")"
 git clone https://github.com/servo/servo
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo apt install python3-pip python3-venv
+cd servo
 ./mach bootstrap
