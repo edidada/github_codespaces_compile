1f8bdcc8766fb03f2d5a666c5947ddfea494d7b8
wdidada <xxx@qq.com>
2024-02-29 08:57:22 +0800

    add servo

---
+++ get_servo.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+git clone https://github.com/servo/servo
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install python3-pip python3-venv
+./mach bootstrap
