8263b21a073c51517d28ea9c6be89f51361d6ec6
wdidada <xxx@qq.com>
2024-03-06 14:47:01 +0800

    modify odb

--- get_odb.sh
+++ get_odb.sh
@@ -4,24 +4,28 @@ sudo apt update
 git clone -b 2.4.0 https://git.codesynthesis.com/odb/libodb.git
 cd libodb
 sudo apt-get install gcc-10-plugin-dev
+./bootstrap
 ./configure
 make
 sudo make install
 cd ../
 git clone -b 2.4.0 https://git.codesynthesis.com/odb/odb.git
 cd odb
+./bootstrap
 ./configure
 make
 sudo make install
 cd ../
 git clone -b 2.4.0 https://git.codesynthesis.com/odb/libodb-mysql.git
 cd libodb-mysql
+./bootstrap
 ./configure
 make
 sudo make install
 cd ../
 git clone -b 2.4.0 https://git.codesynthesis.com/odb/odb-examples.git
 cd odb-examples
+./bootstrap
 ./configure
 make
 sudo make install
