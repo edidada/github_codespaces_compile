6c9c222908c6bbdb282b514f47dfbf42099533fb
wdidada <xxx@qq.com>
2024-03-07 15:21:15 +0800

    add fastdfs

---
+++ get_fastdfs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+sudo apt-get update
+git clone https://github.com/happyfish100/libfastcommon.git
+cd libfastcommon
+git checkout V1.0.56
+./make.sh clean && ./make.sh && ./make.sh install
+cd ../
+git clone https://github.com/happyfish100/fastdfs.git
+cd fastdfs
+git checkout V6.08
+./make.sh clean && ./make.sh && ./make.sh install
+./setup.sh /etc/fdfs
