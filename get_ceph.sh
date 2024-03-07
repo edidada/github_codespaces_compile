dbc10fb4c91be9bdf2d062ba6e0bebd0c787c411
wiseism <141292213+wiseism@users.noreply.github.com>
2024-03-07 07:27:38 +0000

    modify ceph

--- get_ceph.sh
+++ get_ceph.sh
@@ -4,7 +4,7 @@ git clone -b v15.2.17 https://github.com/ceph/ceph.git
 cd ceph
 git submodule update --init --recursive --progress
 sudo apt update
-sudo apt install curl -y
+sudo apt install curl ninja-build -y
 sudo apt install python3-routes -y
 ./install-deps.sh
 ./do_cmake.sh
