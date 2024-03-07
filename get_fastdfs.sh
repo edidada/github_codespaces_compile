49d3c2950913badcd271612cb8e8e29f8faedcbd
wiseism <141292213+wiseism@users.noreply.github.com>
2024-03-07 07:29:34 +0000

    add

--- get_fastdfs.sh
+++ get_fastdfs.sh
@@ -4,10 +4,10 @@ sudo apt-get update
 git clone https://github.com/happyfish100/libfastcommon.git
 cd libfastcommon
 git checkout V1.0.56
-./make.sh clean && ./make.sh && ./make.sh install
+./make.sh clean && ./make.sh && sudo ./make.sh install
 cd ../
 git clone https://github.com/happyfish100/fastdfs.git
 cd fastdfs
 git checkout V6.08
-./make.sh clean && ./make.sh && ./make.sh install
+./make.sh clean && ./make.sh && sudo ./make.sh install
 ./setup.sh /etc/fdfs
