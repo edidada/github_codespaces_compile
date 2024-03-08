5296f4436d424789fc5fc1358a8572e4d401ba98
edidada <sandisks555@gmail.com>
2024-03-08 16:23:31 +0000

    add

--- get_xv6.sh
+++ get_xv6.sh
@@ -6,4 +6,7 @@ make
 sudo apt update
 LC_ALL=C lscpu | grep Virtualization
 sudo apt install qemu qemu-kvm virt-manager bridge-utils -y
+sudo useradd -g $USER libvirt
+sudo useradd -g $USER libvirt-kvm
+sudo systemctl enable libvirtd.service && sudo systemctl start libvirtd.service
 make qemu
