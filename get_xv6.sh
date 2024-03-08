063ac1985302179c1b59348e87d56e5168be23eb
edidada <sandisks555@gmail.com>
2024-03-08 16:20:22 +0000

    add

--- get_xv6.sh
+++ get_xv6.sh
@@ -4,5 +4,6 @@ git clone https://github.com/mit-pdos/xv6-public.git
 cd xv6-public
 make
 sudo apt update
-sudo apt install qemu -y
+LC_ALL=C lscpu | grep Virtualization
+sudo apt install qemu qemu-kvm virt-manager bridge-utils -y
 make qemu
