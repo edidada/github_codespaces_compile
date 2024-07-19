5493259ccdffcd6283c0795d4684f928072b6e61
wdidada <xxx@qq.com>
2024-07-19 10:27:20 +0800

    modify Crow

--- get_crow.sh
+++ get_crow.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt install build-essential zlib1g-dev libncurses5-dev libssl-dev libreadline-dev libsqlite3-dev wget llvm libboost-all-dev -y
 git clone -b v1.0+5 https://github.com/CrowCpp/Crow.git
 cd Crow
 mkdir build && cd build
