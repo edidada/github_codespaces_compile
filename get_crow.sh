0e4ebf4137c1209741db6a3f84db2f3adde9e469
wdidada <xxx@qq.com>
2024-07-19 10:46:27 +0800

    modify Crow

--- get_crow.sh
+++ get_crow.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo apt update
 apt-cache showpkg libboost-all-dev
-sudo apt install libboost-all-dev=1.64.0
-sudo apt install build-essential zlib1g-dev libncurses5-dev libssl-dev libreadline-dev libsqlite3-dev wget llvm -y
+sudo apt install build-essential zlib1g-dev libncurses5-dev libssl-dev libreadline-dev libsqlite3-dev wget llvm libboost-all-dev -y
 git clone -b v1.0+5 https://github.com/CrowCpp/Crow.git
 cd Crow
 mkdir build && cd build
