28423a617254677c3462913b06f59c6f4ec78651
wdidada <xxx@qq.com>
2024-07-24 00:03:35 +0800

    add protobuf

--- get_protobuf.sh
+++ get_protobuf.sh
@@ -2,6 +2,9 @@
 git clone https://github.com/google/protobuf.git
 cd protobuf
 git checkout v3.11.0
+git submodule update --init --recursive
+chmod +x ./autogen.sh
+./autogen.sh
 chmod +x ./configure
 ./configure
 make -j5
