a781a3bfef7021dba238a964b69f5b471dcece2d
wdidada <xxx@qq.com>
2024-07-25 19:37:19 +0800

    modify charon

--- get_charon.sh
+++ get_charon.sh
@@ -3,8 +3,8 @@ chmod +x ./get_tinyrpc.sh
 ./get_tinyrpc.sh
 cd "$(dirname "$0")"
 git clone https://github.com/Gooddbird/charon.git
-cd charon
-cd charon
+cd charon/charon/pb
 protoc --cpp_out=./ charon.proto
+cd ../
 mkdir ../obj
 make -j5
