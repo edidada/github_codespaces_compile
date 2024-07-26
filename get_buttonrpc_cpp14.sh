870c1120295239fae1bb26b63d23d440a53e437b
wdidada <xxx@qq.com>
2024-07-26 16:43:13 +0800

    modify buttonrpc_cpp14

--- get_buttonrpc_cpp14.sh
+++ get_buttonrpc_cpp14.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 sudo apt update
-sudo apt install libzmq3-dev
+sudo apt install libzmq3-dev -y
+dpkg -L libzmq3-dev
 dpkg -s libzmq3-dev | grep Version
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
 git clone https://github.com/edidada/buttonrpc_cpp14.git
 cd buttonrpc_cpp14
 sudo cp *.hpp /usr/local/include
