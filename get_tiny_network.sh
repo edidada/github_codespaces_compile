833423b57013ba0f62175978151ed672abb53b71
wdidada <xxx@qq.com>
2024-07-26 17:03:53 +0800

    modify tiny-network

--- get_tiny_network.sh
+++ get_tiny_network.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 sudo apt-get update
 sudo apt-get install cmake -y
 git clone https://github.com/Shangyizhou/A-Tiny-Network-Library.git
-cd ./A-Tiny-Network-Library && sudo ./build.sh
+cd ./A-Tiny-Network-Library && chmod +x build.sh && sudo ./build.sh
