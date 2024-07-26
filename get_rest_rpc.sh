3e29dbb5745ed7cb42f40685c918d39fe65f6dca
wdidada <xxx@qq.com>
2024-07-26 15:48:51 +0800

    modify rest_rpc

--- get_rest_rpc.sh
+++ get_rest_rpc.sh
@@ -4,6 +4,7 @@ sudo apt update
 sudo apt install libboost-dev -y
 git clone https://github.com/qicosmos/rest_rpc.git
 cd rest_rpc
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/home/runner/work/github_codespaces_compile/github_codespaces_compile/rest_rpc/include
 git checkout v0.12
 cd examples
 mkdir build
