49e8364318b4835fc5b13c5d7e3f2ef8b2b173e3
wdidada <xxx@qq.com>
2024-07-26 16:48:56 +0800

    modify tiny-network

--- get_tiny_network.sh
+++ get_tiny_network.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 sudo apt-get update
 sudo apt-get install cmake -y
-git clone git@github.com:Shangyizhou/tiny-network.git
-cd ./tiny-network && bash build.sh
+git clone https://github.com/Shangyizhou/A-Tiny-Network-Library.git
+cd ./A-Tiny-Network-Library && bash build.sh
