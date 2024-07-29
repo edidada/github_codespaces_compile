5a787fe1fdf0ca41d11de4b82107c47927777a17
wdidada <xxx@qq.com>
2024-07-29 11:48:29 +0800

    modify llvm

--- get_llvm.sh
+++ get_llvm.sh
@@ -6,9 +6,3 @@ mkdir build && cd build
 cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm
 make -j5
 cd ../
-mkdir build_libcxx && cd build_libcxx
-cmake -G Ninja -S runtimes -B build_libcxx -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
-ninja -C build_libcxx cxx cxxabi unwind
-ninja -C build_libcxx check-cxx check-cxxabi check-unwind
-ninja -C build_libcxx install-cxx install-cxxabi install-unwind
-cd ../
