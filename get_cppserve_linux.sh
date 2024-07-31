4485f894b26392ecca4c75905967b8eb61591712
wdidada <xxx@qq.com>
2024-07-31 17:37:00 +0800

    modify Cppserve

--- get_cppserve_linux.sh
+++ get_cppserve_linux.sh
@@ -4,5 +4,9 @@ sudo apt update
 git clone https://github.com/charmber/Cppserve.git
 cd Cppserve
 git checkout 395b0cd5e42813dc968a7942b430a372d8365253
-cmake -S . -B build
-cmake --build build --parallel 4 --config Debug
+# 默认变量值
+BUILD_DIR="build"
+PARALLEL_VALUE="4"
+CONFIG_VALUE="Debug"
+cmake -S . -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR" --parallel "$PARALLEL_VALUE" --config "$CONFIG_VALUE"
