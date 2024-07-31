c9095f1991411536af4ed3a7b18d9f509096c5e2
wdidada <xxx@qq.com>
2024-07-31 18:53:18 +0800

    modify Cppserve

--- get_cppserve_linux.sh
+++ get_cppserve_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 sudo apt update
-git clone https://github.com/charmber/Cppserve.git
+git clone https://github.com/edidada/Cppserve.git
 cd Cppserve
-git checkout 395b0cd5e42813dc968a7942b430a372d8365253
+#git checkout 395b0cd5e42813dc968a7942b430a372d8365253
 # 默认变量值
 BUILD_DIR="build"
 PARALLEL_VALUE="4"
