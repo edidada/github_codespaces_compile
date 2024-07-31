6b3be9be03ab0ea3f87e1b6b031631f5ec4c1e50
wdidada <xxx@qq.com>
2024-07-31 17:33:37 +0800

    modify Cppserve

--- get_cppserve_linux.sh
+++ get_cppserve_linux.sh
@@ -5,4 +5,4 @@ git clone https://github.com/charmber/Cppserve.git
 cd Cppserve
 git checkout 395b0cd5e42813dc968a7942b430a372d8365253
 cmake -S . -B build
-cmake --build
+cmake --build build --parallel 4 --config Debug
