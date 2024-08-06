1d6a825970d13404456b55ca8acea0f6ce10cdd2
wdidada <xxx@qq.com>
2024-08-06 14:30:07 +0800

    modify eastl

--- get_eastl.sh
+++ get_eastl.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 git clone https://github.com/electronicarts/EASTL.git
 cd EASTL
 git checkout 3.21.12
+git submodule update --init --recursive
 mkdir your_build_folder && cd your_build_folder
 cmake ../ -DEASTL_BUILD_TESTS:BOOL=OFF
 cmake --build .
