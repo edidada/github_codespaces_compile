3d80f0d012d8a818ce5bcd54980c31e1ee45933d
wdidada <xxx@qq.com>
2024-08-06 11:20:46 +0800

    modify eastl

--- get_eastl.sh
+++ get_eastl.sh
@@ -5,6 +5,5 @@ git clone https://github.com/electronicarts/EASTL.git
 cd EASTL
 git checkout 3.21.12
 mkdir your_build_folder && cd your_build_folder
-cmake eastl_source_folder -DEASTL_BUILD_TESTS:BOOL=OFF
-cmake --build . --config your_config
-cd test && ctest -C your_config
+cmake ../ -DEASTL_BUILD_TESTS:BOOL=OFF
+cmake --build .
