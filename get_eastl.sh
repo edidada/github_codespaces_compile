a245b747c334922663a17c13f926f228d1176fe7
wdidada <xxx@qq.com>
2024-08-06 14:34:37 +0800

    modify eastl

--- get_eastl.sh
+++ get_eastl.sh
@@ -6,5 +6,5 @@ cd EASTL
 git checkout 3.21.12
 git submodule update --init --recursive
 mkdir your_build_folder && cd your_build_folder
-cmake ../ -DEASTL_BUILD_TESTS:BOOL=OFF
+cmake ../ -DEASTL_BUILD_TESTS:BOOL=ON -DEASTL_BUILD_BENCHMARK:BOOL=ON
 cmake --build .
