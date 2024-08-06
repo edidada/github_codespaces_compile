142592d3304f2fb7b57223ad51c99f18d0f4e547
wdidada <xxx@qq.com>
2024-08-06 11:17:10 +0800

    add eastl

---
+++ get_eastl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+sudo apt-get update
+git clone https://github.com/electronicarts/EASTL.git
+mkdir your_build_folder && cd your_build_folder
+cmake eastl_source_folder -DEASTL_BUILD_TESTS:BOOL=OFF
+cmake --build . --config your_config
+cd test && ctest -C your_config
