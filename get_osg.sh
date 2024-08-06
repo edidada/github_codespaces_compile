c2dae29f90c0846e24456d69985c99b5557f5125
wdidada <xxx@qq.com>
2024-08-06 09:34:22 +0800

    modify OpenSceneGraph

--- get_osg.sh
+++ get_osg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 sudo apt-get update
-sudo apt-get install -y libgl1-mesa-dev
+sudo apt-get install -y libgl1-mesa-dev libglvnd-dev
 git clone https://github.com/openscenegraph/OpenSceneGraph.git
 cd OpenSceneGraph
 git checkout OpenSceneGraph-3.6.2
