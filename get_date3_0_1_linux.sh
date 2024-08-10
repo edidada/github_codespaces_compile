f85b29f8a7a31a1d5c87427d048a4696f1dd44b3
wdidada <xxx@qq.com>
2024-08-10 10:39:19 +0800

    add date3_0_1

---
+++ get_date3_0_1_linux.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+sudo apt update
+cd "$(dirname "$0")"
+git clone https://github.com/HowardHinnant/date.git
+cd date
+git checkout v3.0.1
+mkdir build
+cd build
+cmake -DENABLE_DATE_TESTING=ON -DBUILD_TZ_LIB=ON ../
+cmake --build .
+cmake --install .
