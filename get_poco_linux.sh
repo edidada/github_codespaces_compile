b2a81b46eca0ad76b3643508378fcdd8c10270a7
wdidada <xxx@qq.com>
2024-08-15 16:02:14 +0800

    modify poco

--- get_poco_linux.sh
+++ get_poco_linux.sh
@@ -5,7 +5,9 @@ sudo apt-get install -y openssl libssl-dev git g++ make cmake libmysqlclient-dev
 git clone https://github.com/pocoproject/poco.git
 cd poco
 git checkout poco-1.12.3-release
-cmake -S . -B build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G "Unix Makefiles" -S . -B build
+ls -la build
 cmake --build build
+ls -la build
 #sudo cmake --install build
 sudo cmake --build . --target install
