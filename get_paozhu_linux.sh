a4900f8ba184837e356fa55f444629e47db42cfc
wdidada <xxx@qq.com>
2025-01-17 22:38:49 +0800

    add paozhu

---
+++ get_paozhu_linux.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+sudo apt-get -y update
+sudo apt-get install cmake -y
+sudo apt-get install -y brotli  libbrotli-dev zlib1g-dev
+sudo apt-get install libgd-dev -y
+sudo apt-get install qrencode libqrencode-dev -y
+sudo apt-get install openssl libssl-dev -y
+sudo apt-get install mysql-server -y
+sudo apt-get install mysql-common -y
+sudo apt-get install mysql-client -y
+sudo apt-get install libmysqlclient-dev -y
+git clone https://github.com/hggq/paozhu.git
+cd paozhu
+mkdir build
+cd build
+cmake ..
+make
