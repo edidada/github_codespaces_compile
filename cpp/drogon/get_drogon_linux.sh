8b7d0e0aa0c36f8f1000e625779952fa2202efed
wdidada <xxx@msn.com>
2025-03-10 08:45:33 +0800

    modify drogon sqlpp11

--- cpp/drogon/get_drogon_linux.sh
+++ cpp/drogon/get_drogon_linux.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 sudo apt update
-sudo apt install libjsoncpp-dev uuid-dev zlib1g-dev openssl libssl-dev -y
-sudo apt install libmariadbclient-dev libsqlite3-dev libhiredis-dev -y
+sudo apt install libjsoncpp-dev uuid-dev zlib1g-dev openssl libssl-dev libc-ares-dev libbrotli-dev -y
+sudo apt install libmariadb-dev-compat libmariadb-dev libsqlite3-dev libhiredis-dev -y
 git clone https://github.com/drogonframework/drogon
 cd drogon
 git submodule update --init
