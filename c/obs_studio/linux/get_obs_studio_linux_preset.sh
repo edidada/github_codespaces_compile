2efbb91e3db0e740e8c76eef0b45a690c9231995
wdidada <xxx@msn.com>
2025-03-14 11:16:41 +0800

    ffmpeg

--- c/obs_studio/linux/get_obs_studio_linux_preset.sh
+++ c/obs_studio/linux/get_obs_studio_linux_preset.sh
@@ -15,7 +15,7 @@ cd build
 ninja
 sudo ninja install
 cd ../../
-sudo apt install -y libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev libx264-dev libcurl4-openssl-dev libmbedtls-dev libgl1-mesa-dev libjansson-dev libluajit-5.1-dev python3-dev libx11-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcb-composite0-dev libxcomposite-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-xfixes0-dev swig libcmocka-dev libxss-dev libglvnd-dev libgles2-mesa-dev libwayland-dev libsrt-openssl-dev libpci-dev libpipewire-0.3-dev libqrcodegencpp-dev uthash-dev
+sudo apt install -y ffmpeg libavcodec-dev  libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev libx264-dev libcurl4-openssl-dev libmbedtls-dev libgl1-mesa-dev libjansson-dev libluajit-5.1-dev python3-dev libx11-dev libxcb-randr0-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcb-composite0-dev libxcomposite-dev libxinerama-dev libxcb1-dev libx11-xcb-dev libxcb-xfixes0-dev swig libcmocka-dev libxss-dev libglvnd-dev libgles2-mesa-dev libwayland-dev libsrt-openssl-dev libpci-dev libpipewire-0.3-dev libqrcodegencpp-dev uthash-dev
 #sudo apt install -y \
 #       qt6-base-dev \
 #       qt6-base-private-dev \
@@ -44,6 +44,7 @@ sudo apt install -y qt6-base-dev qt6-base-private-dev qt6-wayland qt6-image-form
 sudo apt-get install -y ffmpeg
 git clone --recursive https://github.com/obsproject/obs-studio.git
 cd obs-studio
+git checkout 31.0.2
 cmake --preset ubuntu
 cmake --build --preset ubuntu
 cmake --install --preset ubuntu
