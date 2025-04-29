a0ddc2b8d8ea3ab0e20c3a3f60131758025ffa42
wdidada <xxx@msn.com>
2025-04-29 20:16:47 +0800

    modify get_java_stringtemplate4_linux

--- java/stringtemplate4/linux/get_java_stringtemplate4_linux.sh
+++ java/stringtemplate4/linux/get_java_stringtemplate4_linux.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 git clone https://github.com/antlr/stringtemplate4.git
 cd stringtemplate4
 git checkout ST4-4.3.4
-mvn clean package
+mvn clean package -D maven.test.skip=true
 ls target -la
