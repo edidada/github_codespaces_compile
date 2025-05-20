8c35274f925a65d9578dc0e410f9be17de8bdbe4
wdidada <xxx@msn.com>
2025-05-20 19:07:14 +0800

    modify zed

--- rust/get_zed.sh
+++ rust/get_zed.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 cd "$(dirname "$0")"
+sudo apt update
+sudo apt install mold -y
+mold --version
+sudo apt install build-essential clang cmake git libgl1 libx11-dev libxkbfile-dev libxrandr-dev libxi-dev libgl1-mesa-dev xorg-dev -y
 git clone https://github.com/zed-industries/zed.git
 cd zed
 cargo run --release
