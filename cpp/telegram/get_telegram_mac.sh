d67c906d6d0a788ac407a2e947d4b70e52ae0729
wdidada <xxx@msn.com>
2025-06-13 17:02:32 +0800

    modify telegram mac

---
+++ cpp/telegram/get_telegram_mac.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+mkdir TBuild
+cd TBuild
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git automake cmake wget pkg-config gnu-tar ninja nasm meson
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+git clone --recursive https://github.com/telegramdesktop/tdesktop.git
+git checkout v5.15.3
+./tdesktop/Telegram/build/prepare/mac.sh
