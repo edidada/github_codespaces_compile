4ef9d5b2e8e17efcee6398bae6877ce6e0348491
wdidada <xxx@msn.com>
2025-06-19 16:51:53 +0800

    modify get_filezilla3 wxWidgets-3.0.1

--- cpp/filezilla3/get_filezilla3_linux2.sh
+++ cpp/filezilla3/get_filezilla3_linux2.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 cd "$(dirname "$0")"
-wget -O wxWidgets-3.2.1.zip https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.1/wxWidgets-3.2.1.zip
-unzip wxWidgets-3.2.1.zip -d ./wxWidgets-3.2.1
-cd ./wxWidgets-3.2.1
+wget -O wxWidgets-3.0.1.zip https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.1/wxWidgets-3.0.1.zip
+unzip wxWidgets-3.0.1.zip -d ./wxWidgets-3.0.1
+cd ./wxWidgets-3.0.1
 ./configure
 make -j$(nproc)
 sudo make install
