390ab032a9253736d64236a532938cb4524e3a48
wdidada <xxx@msn.com>
2025-06-19 12:42:51 +0800

    modify FileZilla3 libgtk2.0-dev

--- cpp/filezilla3/get_filezilla3_linux.sh
+++ cpp/filezilla3/get_filezilla3_linux.sh
@@ -3,7 +3,8 @@ cd "$(dirname "$0")"
 sudo add-apt-repository ppa:mhier/libboost-latest
 sudo apt update
 apt search libboost1
-sudo apt install subversion libgtk2.0-dev libgtk-3-dev xdg-utils libboost1.81-all-dev libwxgtk3.0-gtk3-dev -y
+apt search libwxgtk
+sudo apt install subversion libgtk2.0-dev libgtk-3-dev xdg-utils libboost1.83-all-dev libwxgtk3.0-gtk3-dev -y
 echo "which svn"
 which svn
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
