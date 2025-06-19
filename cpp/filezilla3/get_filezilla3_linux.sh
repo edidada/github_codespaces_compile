b0689b3533334d2f5b949300c621420795c04f73
wdidada <xxx@msn.com>
2025-06-19 14:59:15 +0800

    modify get_filezilla3

--- cpp/filezilla3/get_filezilla3_linux.sh
+++ cpp/filezilla3/get_filezilla3_linux.sh
@@ -10,7 +10,7 @@ which svn
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
 pkg-config --exists gtk+-2.0 && echo "GTK+ 2 is available"
 pkg-config gtk+-2.0 --libs
-tar -xvf files/libfilezilla-0.50.0.tar.xz
+tar -xvf ../../files/libfilezilla-0.50.0.tar.xz
 cd libfilezilla-0.50.0/
 ./configure --prefix=/usr/local
 make -j$(nproc)
