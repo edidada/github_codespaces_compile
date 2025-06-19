49563f905191107002b3c652e06e8e71094ad687
wdidada <xxx@msn.com>
2025-06-19 19:07:00 +0800

    modify get_filezilla3 remove libwxgtk3.2-dev

--- cpp/filezilla3/get_filezilla3_linux1.sh
+++ cpp/filezilla3/get_filezilla3_linux1.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:mhier/libboost-latest
 sudo apt update
 apt search libboost1
 apt search libwxgtk
-sudo apt install subversion nettle-dev libpugixml-dev libgnutls28-dev libgtk2.0-dev libgtk-3-dev xdg-utils libboost1.83-all-dev libwxgtk3.2-dev -y
+sudo apt install subversion nettle-dev libpugixml-dev libgnutls28-dev libgtk2.0-dev libgtk-3-dev xdg-utils libboost1.83-all-dev -y
 echo "which svn"
 which svn
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
