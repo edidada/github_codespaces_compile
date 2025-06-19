9adcc8a888184369c5aca79e41fab83d8d90caad
wdidada <xxx@msn.com>
2025-06-19 13:45:50 +0800

    modify libfilezilla

--- cpp/libfilezilla/get_libfilezilla_linux.sh
+++ cpp/libfilezilla/get_libfilezilla_linux.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cd "$(dirname "$0")"/libboost-latest
+cd "$(dirname "$0")"
 sudo apt update
 apt search libboost1
 apt search libwxgtk
@@ -9,14 +9,3 @@ which svn
 export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
 pkg-config --exists gtk+-2.0 && echo "GTK+ 2 is available"
 pkg-config gtk+-2.0 --libs
-wget -O libfilezilla-0.50.0.tar.xz https://dl3.cdn.filezilla-project.org/libfilezilla/libfilezilla-0.50.0.tar.xz?h=Lafu1Ycp6g8ABpHqaShf2w&x=1750312053
-echo "which libfilezilla"
-ls -la
-tar -xvf libfilezilla-0.50.0.tar.xz
-cd libfilezilla-0.50.0/
-./configure --prefix=/usr/local
-make -j$(nproc)
-sudo make install
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH
-echo "libfilezilla"
-pkg-config --modversion libfilezilla
