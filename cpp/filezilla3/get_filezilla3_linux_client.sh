a5dc4a2839799ad8c94cc60caa93021f0f034489
wdidada <xxx@msn.com>
2025-06-25 16:11:38 +0800

    modify filezilla 3.56.0

--- cpp/filezilla3/get_filezilla3_linux_client.sh
+++ cpp/filezilla3/get_filezilla3_linux_client.sh
@@ -5,6 +5,6 @@ sudo apt-get install libfilezilla-dev libwxbase3.0-dev gnutls-dev libdbus-1-dev 
 svn co https://svn.filezilla-project.org/svn/FileZilla3/tags/3.56.0/ fz
 cd fz
 autoreconf -i
-./configure --with-pugixml=builtin
+CXXFLAGS="-include cstdint -O2 -g" ./configure --with-pugixml=builtin
 make -j4
 sudo make install
