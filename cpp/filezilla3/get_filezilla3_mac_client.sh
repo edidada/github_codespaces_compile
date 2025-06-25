2e3d6f9829210a8f6fc86ef1919a0e6a82326350
wdidada <xxx@msn.com>
2025-06-25 20:29:17 +0800

    modify libdill mac filezilla 3.56.0

--- cpp/filezilla3/get_filezilla3_mac_client.sh
+++ cpp/filezilla3/get_filezilla3_mac_client.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 brew update
-brew install libfilezilla wxwidgets gnutls dbus gtk+3 sqlite gettext
+brew install libfilezilla wxwidgets gnutls dbus gtk+3 sqlite gettext libdill
 # 确保找到 Homebrew 的库和头文件
 export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig:/opt/homebrew/opt/sqlite/lib/pkgconfig:/opt/homebrew/opt/gnutls/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LDFLAGS="-L/opt/homebrew/opt/gnutls/lib -L/opt/homebrew/opt/sqlite/lib -L/opt/homebrew/opt/gettext/lib"
