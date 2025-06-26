e65d517f362a16ae50e7f85e943840f96e4a1bfa
wdidada <xxx@msn.com>
2025-06-26 08:41:25 +0800

    modify libfilezilla-0.41.1.tar.bz2 mac filezilla 3.56.0

--- cpp/filezilla3/get_filezilla3_mac_client.sh
+++ cpp/filezilla3/get_filezilla3_mac_client.sh
@@ -2,6 +2,12 @@
 cd "$(dirname "$0")"
 brew update
 brew install autoconf automake libtool wxwidgets gnutls dbus gtk+3 sqlite gettext svn
+curl -OL https://download.filezilla-project.org/libfilezilla/libfilezilla-0.41.1.tar.bz2
+tar xf libfilezilla-0.41.1.tar.bz2
+cd libfilezilla-0.41.1
+./configure --prefix="$HOME/prefix" --enable-shared --disable-static
+make
+make install
 # 确保找到 Homebrew 的库和头文件
 export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig:/opt/homebrew/opt/sqlite/lib/pkgconfig:/opt/homebrew/opt/gnutls/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LDFLAGS="-L/opt/homebrew/opt/gnutls/lib -L/opt/homebrew/opt/sqlite/lib -L/opt/homebrew/opt/gettext/lib"
