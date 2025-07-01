82f10f1ef5f093e46abb8a08c2d216e2a2ec1d5f
wdidada <xxx@msn.com>
2025-07-01 18:57:41 +0800

    brpc

--- cpp/brpc/get_brpc_1_13_0.sh
+++ cpp/brpc/get_brpc_1_13_0.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 sudo apt update
-sudo apt-get install -y git g++ make libssl-dev libgflags-dev libprotobuf-dev libprotoc-dev protobuf-compiler libleveldb-dev
+sudo apt-get install -y git g++ make libssl-dev libgflags-dev libprotobuf-dev libprotoc-dev protobuf-compiler libleveldb-dev doxygen
 sudo apt-get install -y libsnappy-dev
 git clone https://github.com/apache/brpc.git
 cd brpc
 git checkout 1.10.0
 sh config_brpc.sh --headers=/usr/include --libs=/usr/lib
 make -j4
+doxygen Doxyfile
+cat docs/html/index.html
