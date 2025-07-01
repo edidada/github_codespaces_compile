b973336a70e40c0a6b415bac5754635b47b229aa
wdidada <xxx@msn.com>
2025-07-01 19:43:37 +0800

    brpc

--- cpp/brpc/get_brpc_1_13_0.sh
+++ cpp/brpc/get_brpc_1_13_0.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y git g++ make libssl-dev libgflags-dev libprotobuf-dev li
 sudo apt-get install -y libsnappy-dev
 git clone https://github.com/apache/brpc.git
 cd brpc
-git checkout 1.10.0
+git checkout 1.13.0
 sh config_brpc.sh --headers=/usr/include --libs=/usr/lib
 make -j4
 doxygen Doxyfile
