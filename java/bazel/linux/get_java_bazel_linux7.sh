778d02281c9d08228c231174016977c975812971
wdidada <xxx@msn.com>
2025-07-23 19:51:44 +0800

    bazel7

--- java/bazel/linux/get_java_bazel_linux7.sh
+++ java/bazel/linux/get_java_bazel_linux7.sh
@@ -17,9 +17,9 @@ echo "java version"
 java --version
 javac --version
 cd "$(dirname "$0")"
-wget https://github.com/bazelbuild/bazel/releases/download/8.3.0/bazel-8.3.0-dist.zip
-unzip bazel-8.3.0-dist.zip -d ./bazel-8.3.0-dist
-cd bazel-8.3.0-dist
+wget https://github.com/bazelbuild/bazel/releases/download/7.0.0/bazel-7.0.0-dist.zip
+unzip bazel-7.0.0-dist.zip -d ./bazel-7.0.0-dist
+cd bazel-7.0.0-dist
 wget https://github.com/bazelbuild/bazelisk/releases/download/v1.26.0/bazelisk-linux-amd64
 cp bazelisk-linux-amd64 bazelisk
 chmod +x bazelisk
