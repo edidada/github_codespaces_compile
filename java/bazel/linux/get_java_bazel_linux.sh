2be8ff6efac48cbb9368375f7ccc888124e6d643
wdidada <xxx@msn.com>
2025-07-23 19:12:36 +0800

    bazel

--- java/bazel/linux/get_java_bazel_linux.sh
+++ java/bazel/linux/get_java_bazel_linux.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 wget https://github.com/bazelbuild/bazel/releases/download/8.3.0/bazel-8.3.0-dist.zip
 unzip bazel-8.3.0-dist.zip -d ./bazel-8.3.0-dist
 cd bazel-8.3.0-dist
-bazelisl build //:combine_distfiles
+ ./bazelisk build //:combine_distfiles
