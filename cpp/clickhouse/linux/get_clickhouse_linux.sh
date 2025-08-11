2168466953b65017a780316bcc309c3dee56e99f
wdidada <xxx@msn.com>
2025-08-11 13:45:43 +0800

    modify ClickHouse

--- cpp/clickhouse/linux/get_clickhouse_linux.sh
+++ cpp/clickhouse/linux/get_clickhouse_linux.sh
@@ -4,11 +4,14 @@ sudo apt update
 echo "ubuntu version"
 lsb_release -a
 hostnamectl
+echo "rustc version"
+rustc --version
+cargo --version
 git clone https://github.com/ClickHouse/ClickHouse.git
 cd ClickHouse
 git checkout v24.6.6.6-stable
 git submodule update --init
-sudo apt install ccache clang lldb -y
+sudo apt install ccache clang lldb nasm yasm doxygen python3-pygments python3-yaml -y
 mkdir build
 cd build
 cmake ../
