b6bd1f1093d1fbb76263a18ec87090e4fa0b9403
wdidada <xxx@qq.com>
2025-09-29 00:25:01 +0800

    modify iceberg cpp

--- cpp/iceberg_cpp/linux/get_iceberg_cpp_linux.sh
+++ cpp/iceberg_cpp/linux/get_iceberg_cpp_linux.sh
@@ -5,5 +5,4 @@ cd iceberg-cpp
 git checkout v0.1.0
 cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=/usr/local/iceberg-cpp -DICEBERG_BUILD_STATIC=ON -DICEBERG_BUILD_SHARED=ON
 cmake --build build
-ctest --test-dir build --output-on-failure
 cmake --install build
