3df396aea252d41994e5f1f3d7f33f5d69f18c51
yyyXXX <yyyXXX@outlook.com>
2025-09-29 08:48:31 +0800

    iceberg_cpp_macos

--- cpp/iceberg_cpp/macos/get_iceberg_cpp_macos.sh
+++ cpp/iceberg_cpp/macos/get_iceberg_cpp_macos.sh
@@ -4,4 +4,4 @@ cd iceberg-cpp
 git checkout v0.1.0
 cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=/usr/local -DICEBERG_BUILD_STATIC=ON -DICEBERG_BUILD_SHARED=ON
 cmake --build build
-cmake --install build
+sudo cmake --install build
