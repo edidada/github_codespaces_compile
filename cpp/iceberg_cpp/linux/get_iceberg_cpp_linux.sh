42e716526dcf9cc00c7ceb72c6e0cdb0ddfab752
yyyXXX <yyyXXX@outlook.com>
2025-09-29 08:34:31 +0800

    iceberg_cpp_linux

--- cpp/iceberg_cpp/linux/get_iceberg_cpp_linux.sh
+++ cpp/iceberg_cpp/linux/get_iceberg_cpp_linux.sh
@@ -3,6 +3,9 @@ sudo apt update
 git clone https://github.com/apache/iceberg-cpp.git
 cd iceberg-cpp
 git checkout v0.1.0
+sudo mkdir /usr/local/iceberg-cpp/bin
+sudo mkdir /usr/local/iceberg-cpp/include
+sudo mkdir /usr/local/iceberg-cpp/lib
 cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=/usr/local/iceberg-cpp -DICEBERG_BUILD_STATIC=ON -DICEBERG_BUILD_SHARED=ON
 cmake --build build
 cmake --install build
