bfed006536c3a250f5e505b0f352bc0ef9ce3e13
yyyXXX <yyyXXX@outlook.com>
2025-10-16 19:18:02 +0800

    cpp 3fs

--- cpp/3fs/linux/get_3fs_linux.sh
+++ cpp/3fs/linux/get_3fs_linux.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 sudo apt update
-apt install cmake libuv1-dev liblz4-dev liblzma-dev libdouble-conversion-dev libdwarf-dev libunwind-dev libaio-dev libgflags-dev libgoogle-glog-dev libgtest-dev libgmock-dev clang-format-14 clang-14 clang-tidy-14 lld-14 libgoogle-perftools-dev google-perftools libssl-dev gcc-12 g++-12 libboost-all-dev build-essential -y
+apt install libboost-all-dev cmake libuv1-dev liblz4-dev liblzma-dev libdouble-conversion-dev libdwarf-dev libunwind-dev libaio-dev libgflags-dev libgoogle-glog-dev libgtest-dev libgmock-dev clang-format-14 clang-14 clang-tidy-14 lld-14 libgoogle-perftools-dev google-perftools libssl-dev gcc-12 g++-12 libboost-all-dev build-essential -y
+ls /usr/include/boost
+ls /usr/lib/x86_64-linux-gnu | grep boost
 git clone https://github.com/deepseek-ai/3fs
 cd 3fs
 git submodule update --init --recursive
