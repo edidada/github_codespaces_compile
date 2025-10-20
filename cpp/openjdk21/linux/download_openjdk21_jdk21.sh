665beddab510786c0709bce7c795232717cbb382
yyyXXX <yyyXXX@outlook.com>
2025-10-20 12:20:03 +0800

    modify cpp build jdk21

--- cpp/openjdk21/linux/download_openjdk21_jdk21.sh
+++ cpp/openjdk21/linux/download_openjdk21_jdk21.sh
@@ -8,13 +8,13 @@ sudo apt install -y libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev 
 wget https://codeload.github.com/openjdk/jdk21/zip/refs/heads/master -O jdk21-master.zip
 unzip jdk21-master.zip
 wget https://download.java.net/openjdk/jdk20/ri/openjdk-20+36_linux-x64_bin.tar.gz
-tar -zxvf openjdk-20+36_linux-x64_bin.tar.gz -C /workspaces/github_codespaces_compile/
+tar -zxvf openjdk-20+36_linux-x64_bin.tar.gz -C /home/runner/work/github_codespaces_compile/github_codespaces_compile/
 cd jdk21-master
-export JAVA_HOME=/workspaces/github_codespaces_compile/jdk-20
+export JAVA_HOME=/home/runner/work/github_codespaces_compile/github_codespaces_compile/jdk-20
 
 # 明确指定构建系统类型（修复config.sub错误）
 BUILD_SYSTEM=$(build-aux/config.guess)
-bash configure --with-num-cores=12 --with-memory-size=20480 --with-boot-jdk=/workspaces/github_codespaces_compile/jdk-20 --with-target-bits=64 --with-jvm-variants=server --with-debug-level=slowdebug -disable-warnings-as-errors  --with-toolchain-type=gcc
+bash configure --with-num-cores=12 --with-memory-size=20480 --with-boot-jdk=/home/runner/work/github_codespaces_compile/github_codespaces_compile/jdk-20 --with-target-bits=64 --with-jvm-variants=server --with-debug-level=slowdebug -disable-warnings-as-errors  --with-toolchain-type=gcc
 make CONF=linux-x86_64-server-slowdebug
 find build/ -name "java" -type f
 # 典型路径：build/linux-x86_64-server-slowdebug/jdk/bin/java
