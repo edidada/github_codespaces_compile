7db4dbff508d566b2998ff4bad0b2192c5c5eae2
yyyXXX <yyyXXX@outlook.com>
2025-10-23 09:11:26 +0800

    c/tengine

--- c/tengine/linux/get_tengine.sh
+++ c/tengine/linux/get_tengine.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt install -y initscripts libc-bin libgd2-xpm-dev libgeoip-dev libxslt1-dev libpcre++0 libpcre++-dev liblua5.1-0-dev libssl-dev lua5.1 openssl passwd libperl-dev libxml2 libxml2-dev libxslt1-dev libxslt-dev
 git clone -b tengine-2.2.2 https://github.com/alibaba/tengine.git
 cd tengine
-./configure --enable-mods-static=all --with-ipv6 --with-http_v2_module
+./configure --enable-mods-static=all --with-ipv6 --with-http_v2_module --with-libxml2=/usr/include/libxml2 --with-libxslt=/usr/include/libxslt
 make -j$(nproc)
 sudo make install
