8af944d7c59a3a531e7d915f2a18d9adb674afbb
yyyXXX <yyyXXX@outlook.com>
2025-10-23 09:35:01 +0800

    c/tengine

--- c/tengine/linux/get_tengine.sh
+++ c/tengine/linux/get_tengine.sh
@@ -2,8 +2,13 @@
 cd "$(dirname "$0")"
 sudo apt-get update
 sudo apt install -y initscripts libc-bin libgd2-xpm-dev libgeoip-dev libxslt1-dev libpcre++0 libpcre++-dev liblua5.1-0-dev libssl-dev lua5.1 openssl passwd libperl-dev libxml2 libxml2-dev libxslt1-dev libxslt-dev
+git config advice.detachedHead false
+# 设置环境变量让 configure 能找到库
+export XML2_CFLAGS="-I/usr/include/libxml2"
+export XML2_LIBS="-lxml2"
+export XSLT_CFLAGS="-I/usr/include/libxslt"
+export XSLT_LIBS="-lxslt"
 git clone -b tengine-2.2.2 https://github.com/alibaba/tengine.git
 cd tengine
-./configure --enable-mods-static=all --with-ipv6 --with-http_v2_module --with-libxml2=/usr/include/libxml2 --with-libxslt=/usr/include/libxslt
-make -j$(nproc)
+./configure --enable-mods-static=all --with-ipv6 --with-http_v2_module
 sudo make install
---
+++ c/tengine/linux/get_tengine_3_1_0.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+sudo apt-get update
+sudo apt install -y initscripts libc-bin libgd2-xpm-dev libgeoip-dev libxslt1-dev libpcre++0 libpcre++-dev liblua5.1-0-dev libssl-dev lua5.1 openssl passwd libperl-dev libxml2 libxml2-dev libxslt1-dev libxslt-dev
+git clone -b tengine-3.1.0 https://github.com/alibaba/tengine.git
+cd tengine
+./configure --enable-mods-static=all --with-ipv6 --with-http_v2_module --without-http_xslt_module
+make -j$(nproc)
+sudo make install
