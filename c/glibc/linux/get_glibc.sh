bf4012767fecb328acfcff8e198056a06fbfa318
yyyXXX <yyyXXX@outlook.com>
2025-10-28 20:55:23 +0800

    c/glibc

--- c/glibc/linux/get_glibc.sh
+++ c/glibc/linux/get_glibc.sh
@@ -8,7 +8,10 @@ tar xzf glibc-2.29.tar.gz
 cd glibc-2.29
 mkdir build
 cd build
-../configure --prefix=/opt/glibc-2.29
-make -j4  # 使用4个并行任务编译以加快速度
+../configure --prefix=/opt/glibc-2.29 \
+             --disable-werror \          # 显式禁用-Werror
+             --enable-kernel=3.2 \       # 指定最低支持的内核版本
+             --with-binutils=/usr/bin    # 确保使用系统binutils
+make -j$(nproc)
 sudo make install
 /opt/glibc-2.29/lib/ld-linux-x86-64.so.2 --version
