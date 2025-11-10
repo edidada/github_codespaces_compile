4487abca51df8a9591dfcfeee10a31f583f8d28c
yyyXXX <yyyXXX@outlook.com>
2025-11-10 20:28:42 +0800

    delete

--- c/bison/linux/3_8_2/get_bison_3_8_2.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo apt-get update
-cd "$(dirname "$0")"
-git clone https://github.com/akimd/bison.git
-cd bison
-git checkout v3.8.2
-git submodule update --init
-./bootstrap
--- cpp/quantlibxl/linux/1_2_5/get_quantlib_cpp_1_2_5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y build-essential libboost-all-dev
-git clone https://github.com/eehlers/QuantLibXL.git
-cd QuantLibXL
