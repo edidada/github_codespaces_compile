c67d8af72e8ba2c320a9be59e47f368939a230a4
yyyXXX <yyyXXX@outlook.com>
2025-11-13 19:28:43 +0800

    cpp/benchmark/linux/1_9_4 bazel

--- cpp/benchmark/linux/1_9_4/get_benchmark_linux_1_9_4.sh
+++ cpp/benchmark/linux/1_9_4/get_benchmark_linux_1_9_4.sh
@@ -10,7 +10,14 @@ cargo --version
 git clone https://github.com/google/benchmark.git
 cd benchmark
 git checkout v1.9.4
+echo "cmake build"
 mkdir build && cd build
 cmake ../
 make
 sudo make install
+echo "bazel build"
+cd ../
+wget https://github.com/bazelbuild/bazelisk/releases/latest/download/bazelisk-linux-amd64
+chmod +x bazelisk-linux-amd64
+sudo mv bazelisk-linux-amd64 /usr/local/bin/bazelisk
+bazelisk build
