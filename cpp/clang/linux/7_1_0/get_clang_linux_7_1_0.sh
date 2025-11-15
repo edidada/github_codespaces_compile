ccc9131f041922b82238163ec8d3402c1af4d2f9
yyyXXX <yyyXXX@outlook.com>
2025-11-15 14:34:32 +0800

    cpp/clang/linux/7_1_0

--- cpp/clang/linux/7_1_0/get_clang_linux_7_1_0.sh
+++ cpp/clang/linux/7_1_0/get_clang_linux_7_1_0.sh
@@ -12,6 +12,7 @@ cd llvm-project
 git checkout llvmorg-7.1.0
 mkdir build && cd build
 cmake ../llvm \
+  -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_PROJECTS="clang" \
   -DCMAKE_INSTALL_PREFIX=/usr/local/llvm-7.1.0
