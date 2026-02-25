14df53b5c86d7bef9fc9b28b1aa0586a83fd6fec
yyyXXX <yyyXXX@outlook.com>
2026-02-25 15:12:13 +0800

    modify go/podman/linux/5_8_0

--- go/podman/5_8_0/linux/get_podman_linux_5_8_0.sh
+++ go/podman/5_8_0/linux/get_podman_linux_5_8_0.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 cd "$(dirname "$0")"
+sudo apt update
+sudo apt install -y \
+    libsqlite3-dev \
+    libgpgme-dev \
+    libseccomp-dev \
+    libdevmapper-dev \
+    btrfs-progs \
+    gcc \
+    make \
+    pkg-config
 git clone https://github.com/containers/podman.git
 cd podman
 git checkout v5.8.0
