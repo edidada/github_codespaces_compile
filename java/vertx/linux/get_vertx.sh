b561d57ad82efba20142bd23e2b0a98a70711dbb
someXXYY <betaXXX@gmail.com>
2026-09-17 20:56:30 +0800

    force Gradle wrapper transport to HTTPS

--- java/vertx/linux/get_vertx.sh
+++ java/vertx/linux/get_vertx.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 git clone -b v2.1.4 https://github.com/eclipse-vertx/vert.x.git
 cd vert.x
-sed -i 's#http\\://services.gradle.org#https\\://services.gradle.org#' gradle/wrapper/gradle-wrapper.properties
+sed -i 's#http#https#g' gradle/wrapper/gradle-wrapper.properties
 for attempt in 1 2 3; do
   ./gradlew build -x test -x integrationTest && exit 0
   rm -rf "$HOME/.gradle/wrapper/dists/gradle-1.7-bin"
