c484a1c42bcc2ea8ec080ddb78dd0682b7b4c180
someXXYY <betaXXX@gmail.com>
2026-09-17 20:12:18 +0800

    fix fcp argument collection type inference

--- rust/fcp/linux/get_rust_fcp.sh
+++ rust/fcp/linux/get_rust_fcp.sh
@@ -3,5 +3,6 @@ cd "$(dirname "$0")"
 git clone -b v0.2.1 https://github.com/Svetlitski/fcp.git
 cd fcp
 sed -i 's/err));/err))/' src/filesystem.rs
+sed -i 's/env::args().skip(1).collect()/env::args().skip(1).collect::<Vec<_>>()/' src/main.rs
 cargo build --release
 cargo test
