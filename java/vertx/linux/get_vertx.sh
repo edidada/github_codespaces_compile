ebe970525e7b386d88e3188a2448afd60d305d1f
someXXYY <betaXXX@gmail.com>
2026-09-17 21:01:12 +0800

    use HTTPS for Vert.x Maven repository

--- java/vertx/linux/get_vertx.sh
+++ java/vertx/linux/get_vertx.sh
@@ -3,6 +3,7 @@ cd "$(dirname "$0")"
 git clone -b v2.1.4 https://github.com/eclipse-vertx/vert.x.git
 cd vert.x
 sed -i 's#http#https#g' gradle/wrapper/gradle-wrapper.properties
+find . -type f \( -name '*.gradle' -o -name 'pom.xml' \) -exec sed -i 's#http://repo1.maven.org#https://repo1.maven.org#g' {} +
 for attempt in 1 2 3; do
   ./gradlew build -x test -x integrationTest && exit 0
   rm -rf "$HOME/.gradle/wrapper/dists/gradle-1.7-bin"
