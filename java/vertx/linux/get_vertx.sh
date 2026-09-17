f78276bba1abdd760576bd73941ec06caa653b46
someXXYY <betaXXX@gmail.com>
2026-09-17 20:49:04 +0800

    retry Vertx Gradle download after corrupted cache

--- java/vertx/linux/get_vertx.sh
+++ java/vertx/linux/get_vertx.sh
@@ -2,4 +2,8 @@
 cd "$(dirname "$0")"
 git clone -b v2.1.4 https://github.com/eclipse-vertx/vert.x.git
 cd vert.x
-./gradlew build -x test -x integrationTest
+for attempt in 1 2 3; do
+  ./gradlew build -x test -x integrationTest && exit 0
+  rm -rf "$HOME/.gradle/wrapper/dists/gradle-1.7-bin"
+done
+exit 1
