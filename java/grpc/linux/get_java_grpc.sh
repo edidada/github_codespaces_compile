fc44b2929e1b2b5c813ace5ea062b1e4647df719
someXXYY <betaXXX@gmail.com>
2026-09-17 21:51:14 +0800

    install Protobuf headers for gRPC Java compiler

--- java/grpc/linux/get_java_grpc.sh
+++ java/grpc/linux/get_java_grpc.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd "$(dirname "$0")"
 sudo apt-get update
+sudo apt-get install -y libprotobuf-dev
 git clone -b v1.39.0 https://github.com/grpc/grpc-java.git
 cd grpc-java
 ./gradlew check :grpc-all:jacocoTestReport
