21171bbf9f8de6c29f54c19f4df159986669288f
someXXYY <betaXXX@gmail.com>
2026-09-17 21:17:02 +0800

    force Maven Central HTTPS replacement

--- java/vertx/linux/get_vertx.sh
+++ java/vertx/linux/get_vertx.sh
@@ -6,6 +6,7 @@ sed -i 's#http#https#g' gradle/wrapper/gradle-wrapper.properties
 find . -type f \( -name '*.gradle' -o -name 'pom.xml' \) -exec sed -i 's#http://#https://#g' {} +
 sed -i 's#http://#https://#g' build.gradle settings.gradle 2>/dev/null || true
 grep -RIl 'http://' . | xargs -r sed -i 's#http://#https://#g'
+find . -type f -exec sed -i 's#http://repo1.maven.org#https://repo1.maven.org#g' {} +
 for attempt in 1 2 3; do
   ./gradlew build -x test -x integrationTest && exit 0
   rm -rf "$HOME/.gradle/wrapper/dists/gradle-1.7-bin"
