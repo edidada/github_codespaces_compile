764536f87342df6d607f46f458a216099f856e08
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 14:28:40 +0800

    fix: adapt Charon to pinned TinyRPC logging API

--- cpp/charon_v/linux/latest/get_charon.sh
+++ cpp/charon_v/linux/latest/get_charon.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
-chmod +x ./cpp/tinyrpc/linux/get_tinyrpc.sh
-./cpp/tinyrpc/linux/get_tinyrpc.sh
+#!/usr/bin/env bash
+set -euo pipefail
+chmod +x ./cpp/tinyrpc_v/linux/latest/get_tinyrpc.sh
+./cpp/tinyrpc_v/linux/latest/get_tinyrpc.sh
 cd "$(dirname "$0")"
 git clone https://github.com/Gooddbird/charon.git
+find charon -type f \( -name '*.cc' -o -name '*.h' \) -exec sed -i \
+  -e 's/AppInfoLog/InfoLog/g' \
+  -e 's/AppErrorLog/ErrorLog/g' {} +
 cd charon/charon/pb
 protoc --cpp_out=./ charon.proto
 cd ../
