119c3d6023e592c859eec83acf766d6ce43981fd
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 09:16:41 +0800

    Add cpp/pistache_v/linux/latest build script

---
+++ cpp/pistache_v/linux/latest/build_pistache_v.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+git clone --depth 1  https://github.com/pistacheio/pistache.git project
+cd project
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --parallel
