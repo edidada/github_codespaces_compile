5ca37a34aefd3dfae6138d27cad74264dd7ae531
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 14:52:49 +0800

    fix: complete Charon logging macro compatibility

--- cpp/charon_v/linux/latest/get_charon.sh
+++ cpp/charon_v/linux/latest/get_charon.sh
@@ -5,7 +5,9 @@ chmod +x ./cpp/tinyrpc_v/linux/latest/get_tinyrpc.sh
 cd "$(dirname "$0")"
 git clone https://github.com/Gooddbird/charon.git
 find charon -type f \( -name '*.cc' -o -name '*.h' \) -exec sed -i \
+  -e 's/AppDebugLog/DebugLog/g' \
   -e 's/AppInfoLog/InfoLog/g' \
+  -e 's/AppWarnLog/WarnLog/g' \
   -e 's/AppErrorLog/ErrorLog/g' {} +
 cd charon/charon/pb
 protoc --cpp_out=./ charon.proto
