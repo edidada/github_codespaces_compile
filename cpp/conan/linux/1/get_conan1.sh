11a980637200a8c126809ee0523d6563d7ea900e
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 09:07:40 +0800

    Use valid Conan 1 install workflow

--- cpp/conan/linux/1/get_conan1.sh
+++ cpp/conan/linux/1/get_conan1.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-sudo update
-sudo apt-get install autoconf automake libtool curl make unzip -y
+set -euo pipefail
+sudo apt-get update
+sudo apt-get install -y autoconf automake libtool curl make unzip
 pip3 install conan==1.62.0
 conan profile update settings.compiler.libcxx=libstdc++11 default
 git clone https://github.com/conan-io/examples.git
 cd examples/libraries/poco/md5
 rm -rf build
 mkdir build && cd build
-conan install ..
-conan build ..
+conan install .. -s build_type=Release
 cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 ./bin/md5
