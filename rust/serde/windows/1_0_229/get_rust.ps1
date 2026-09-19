a73865b424cbec7538efd1a568ea704dba79fa06
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 15:07:44 +0800

    feat: add rust/serde/windows/1_0_229 Rust build

---
+++ rust/serde/windows/1_0_229/get_rust.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+Set-Location $PSScriptRoot
+rustup toolchain install stable --profile minimal
+rustup default stable
+git clone --depth 1 --branch v1.0.229 https://github.com/serde-rs/serde.git project
+Set-Location project
+cargo build --workspace --release
