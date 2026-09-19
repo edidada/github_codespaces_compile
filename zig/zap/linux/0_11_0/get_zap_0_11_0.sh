8cff3741295f5071f3056187850eca9d8be59726
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 08:37:45 +0800

    Use Zig version required by Zap v0.11

--- zig/zap/linux/0_11_0/get_zap_0_11_0.sh
+++ zig/zap/linux/0_11_0/get_zap_0_11_0.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install -y gettext autopoint gperf xz-utils
 cd "$(dirname "$0")"
-ZIG_VERSION=0.11.0
+ZIG_VERSION=0.15.1
 ZIG_DIR="zig-linux-x86_64-${ZIG_VERSION}"
 if ! command -v zig >/dev/null 2>&1; then
   curl -fsSLO "https://ziglang.org/download/${ZIG_VERSION}/${ZIG_DIR}.tar.xz"
