895abf172aae4bccb5ca682d860a587256e63542
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 10:25:37 +0800

    feat: add cpp/whisper_cpp/linux/latest AI build

---
+++ cpp/whisper_cpp/linux/latest/get_ai.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+sudo apt-get update && sudo apt-get install -y git cmake ninja-build build-essential
+git clone --depth 1 --recursive  https://github.com/ggml-org/whisper.cpp.git project
+cd project
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DWHISPER_BUILD_TESTS=OFF -DWHISPER_BUILD_EXAMPLES=OFF
+cmake --build build --config Release --parallel 2
