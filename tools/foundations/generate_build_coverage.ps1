167e0f4f74b4b8ead720ecbb9132d6dbe72107d6
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 22:33:56 +0800

    fix(apache): provision supported CouchDB and legacy PHP runtimes

--- tools/foundations/generate_build_coverage.ps1
+++ tools/foundations/generate_build_coverage.ps1
@@ -82,13 +82,13 @@ $repositoryOverrides = @{
     'apache:teaclave' = 'https://github.com/apache/teaclave-sgx-sdk.git'
 }
 $buildCommandOverrides = @{
-    'apache:attic-buildr' = 'bundle exec rake test'
+    'apache:attic-buildr' = 'bundle install && bundle exec rake test'
     'apache:brooklyn' = 'mvn -B -ntp -DskipITs test'
     'apache:flex' = 'ant -Dbuild.noprompt=true modules'
     'apache:incubator-pouchdb' = 'npm install && npm run build && npm run test-unit'
     'apache:libcloud' = 'sudo apt-get update && sudo apt-get install -y libvirt-dev pkg-config && python -m pip install --upgrade tox && tox -e py3.12'
     'apache:logging-log4cxx' = 'sudo apt-get update && sudo apt-get install -y libapr1-dev libaprutil1-dev && cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON && cmake --build build --parallel 2 && ctest --test-dir build --output-on-failure'
-    'apache:logging-log4php' = 'curl -fsSL https://phar.phpunit.de/phpunit-4.8.36.phar -o /tmp/phpunit-log4php.phar && docker run --rm -v "$PWD:/work" -v /tmp/phpunit-log4php.phar:/tmp/phpunit.phar:ro -w /work php:5.6-cli bash -euc ''docker-php-ext-install sockets; php /tmp/phpunit.phar -c phpunit.xml'''
+    'apache:logging-log4php' = 'curl -fsSL https://phar.phpunit.de/phpunit-4.8.36.phar -o /tmp/phpunit-log4php.phar && docker run --rm -v "$PWD:/work" -v /tmp/phpunit-log4php.phar:/tmp/phpunit.phar:ro -w /work php:5.6-cli bash -euc ''docker-php-ext-install sockets zlib; php /tmp/phpunit.phar -c phpunit.xml'''
     'apache:logging-log4net' = 'dotnet build ./src/log4net.sln && dotnet test ./src/log4net.sln --no-build'
     'apache:opendal' = 'if ! command -v protoc >/dev/null; then sudo apt-get update && sudo apt-get install -y protobuf-compiler; fi; cargo test -p opendal --lib'
     'apache:serf' = 'sudo apt-get update && sudo apt-get install -y libapr1-dev libaprutil1-dev libssl-dev zlib1g-dev && cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON && cmake --build build --parallel 2 && ctest --test-dir build --output-on-failure'
@@ -97,7 +97,7 @@ $buildCommandOverrides = @{
     'apache:training' = './mvnw -B -ntp -DskipITs verify'
     'apache:vcl' = 'find web -type f -name "*.php" -print0 | xargs -0 -n1 php -l && perl -c managementnode/bin/install_perl_libs.pl'
     'apache:xmlgraphics-batik' = 'mvn -B -ntp -DskipITs -pl "!batik-test-old" test'
-    'apache:couchdb' = 'sudo apt-get update && sudo apt-get install -y erlang elixir help2man libicu-dev libmozjs-115-dev python3 python3-venv && ./configure --spidermonkey-version 115 --disable-docs --disable-fauxton && make eunit'
+    'apache:couchdb' = 'sudo apt-get update && sudo apt-get install -y help2man libicu-dev libmozjs-115-dev python3 python3-venv && ./configure --spidermonkey-version 115 --disable-docs --disable-fauxton && make eunit'
     'apache:yetus' = 'mvn -B -ntp -DskipITs verify'
 }
 $projectNotes = @{
@@ -250,7 +250,15 @@ The branch and path follow ``编程语言/软件名称/操作系统/版本``. A 
         uses: ruby/setup-ruby@v1
         with:
           ruby-version: '2.7'
-          bundler-cache: true
+          bundler-cache: false
+"@
+        } elseif ($overrideKey -eq 'apache:couchdb') {
+            $setupSteps = @"
+      - name: Set up supported Erlang and Elixir
+        uses: erlef/setup-beam@v1
+        with:
+          otp-version: '27.3.4.6'
+          elixir-version: '1.17.3'
 "@
         } elseif ($overrideKey -eq 'apache:brooklyn') {
             $setupSteps = @"
