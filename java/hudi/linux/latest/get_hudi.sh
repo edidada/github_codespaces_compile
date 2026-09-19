9d50efb7764bdf106b32a154abddde9499c09cf9
somessyy <yyoiisdfasd@outlook.com>
2026-09-19 10:20:47 +0800

    feat: add java/hudi/linux/latest build

---
+++ java/hudi/linux/latest/get_hudi.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")"
+sudo apt-get update
+sudo apt-get install -y git curl build-essential 
+git clone --depth 1 --recursive  https://github.com/apache/hudi.git hudi
+cd hudi
+./mvnw -B -DskipTests -Dcheckstyle.skip -Drat.skip -pl hudi-common -am package
