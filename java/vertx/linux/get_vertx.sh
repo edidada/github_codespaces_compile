79e6c70971f82657568c283f86a8bf8b0c9722d1
someXXYY <betaXXX@gmail.com>
2026-09-21 00:59:55 +0800

    fix Vert.x Gradle Maven Central HTTPS

--- java/vertx/linux/get_vertx.sh
+++ java/vertx/linux/get_vertx.sh
@@ -5,6 +5,7 @@ cd vert.x
 sed -i 's#http#https#g' gradle/wrapper/gradle-wrapper.properties
 find . -type f \( -name '*.gradle' -o -name 'pom.xml' \) -exec sed -i 's#http://#https://#g' {} +
 sed -i 's#http://#https://#g' build.gradle settings.gradle 2>/dev/null || true
+sed -i "s#mavenCentral()#maven { url 'https://repo.maven.apache.org/maven2' }#g" build.gradle
 grep -RIl 'http://' . | xargs -r sed -i 's#http://#https://#g'
 find . -type f -exec sed -i 's#http://repo1.maven.org#https://repo1.maven.org#g' {} +
 for attempt in 1 2 3; do
