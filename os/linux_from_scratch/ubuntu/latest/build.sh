39149f103283fbecb00141a6713b16681a262b86
someXXYY <betaXXX@gmail.com>
2026-09-22 00:12:09 +0800

    skip interactive LFS terminal check in CI

--- os/linux_from_scratch/ubuntu/latest/build.sh
+++ os/linux_from_scratch/ubuntu/latest/build.sh
@@ -24,6 +24,9 @@ run_in_container 'cd /vagrant && bash /vagrant/02-prepare-for-the-build.sh'
 run_in_container "su - vagrant -c 'source /vagrant/jhalfs/jhalfs.sh && cd \$LFS/jhalfs/book-source && sed -i \"s/--disable-static/--disable-static --host=none-linux-gnu/\" chapter08/gmp.xml'"
 run_in_container "su - vagrant -c 'source /vagrant/jhalfs/jhalfs.sh && cd \$LFS/jhalfs/lfs-commands/chapter08 && sed -i \"/make -k check/d; /Timed out/d; /test_summary/d\" 8*-glibc 8*-gcc'"
 run_in_container "su - vagrant -c 'source /vagrant/jhalfs/jhalfs.sh && cd \$LFS/jhalfs/lfs-commands/chapter10 && sed -i \"s|cp -v ../kernel-config.*|make defconfig|\" 10*-kernel'"
+# This jhalfs target requires an interactive 80x24 terminal, which is absent
+# on hosted CI.  All build targets remain enabled.
+run_in_container "su - vagrant -c 'source /vagrant/jhalfs/jhalfs.sh && cd \$LFS/jhalfs && sed -i \"/^all:/s/ck_terminal//\" Makefile'"
 run_in_container "su - vagrant -c 'source /vagrant/jhalfs/jhalfs.sh && cd \$LFS/jhalfs && make all'"
 run_in_container 'source /vagrant/jhalfs/jhalfs.sh && cd /vagrant && bash 10.4-configure-grub.sh'
 
