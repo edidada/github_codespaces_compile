2c471a91c68be52a1b79b643a5e8576fa00c23f7
someXXYY <betaXXX@gmail.com>
2026-09-21 23:03:42 +0800

    build Aeron all jar before tests

--- java/aeron/linux/latest/build.sh
+++ java/aeron/linux/latest/build.sh
@@ -5,7 +5,7 @@ export PROJECT_NAME='Aeron'
 export PROJECT_REPOSITORY='https://github.com/aeron-io/aeron.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
-export PROJECT_BUILD_COMMAND="sed -i 's/^networkTimeout=.*/networkTimeout=60000/' gradle/wrapper/gradle-wrapper.properties && ./gradlew --no-daemon test"
+export PROJECT_BUILD_COMMAND="sed -i 's/^networkTimeout=.*/networkTimeout=60000/' gradle/wrapper/gradle-wrapper.properties && ./gradlew --no-daemon :aeron-all:jar test"
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec "$repository_root/tools/foundations/build_project.sh"
