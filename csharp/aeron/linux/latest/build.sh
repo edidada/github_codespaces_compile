272c5af45e67dc0acebf2237c5e6cd361362c857
someXXYY <betaXXX@gmail.com>
2026-09-21 22:44:16 +0800

    build Aeron .NET library

--- csharp/aeron/linux/latest/build.sh
+++ csharp/aeron/linux/latest/build.sh
@@ -6,7 +6,7 @@ export PROJECT_REPOSITORY='https://github.com/AdaptiveConsulting/Aeron.NET.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
 export PROJECT_BUILD_ROOT='src'
-export PROJECT_BUILD_COMMAND='dotnet test Adaptive.Aeron.Tests/Adaptive.Aeron.Tests.csproj --configuration Release'
+export PROJECT_BUILD_COMMAND='dotnet build Adaptive.Aeron/Adaptive.Aeron.csproj --configuration Release'
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec "$repository_root/tools/foundations/build_project.sh"
