49c19eedb9874b0e8aad0716555f322cb7c6ee2b
someXXYY <betaXXX@gmail.com>
2026-09-23 22:58:38 +0800

    build zxing android profile from repository root

--- java/zxing/android/latest/build.sh
+++ java/zxing/android/latest/build.sh
@@ -4,7 +4,7 @@ export PROJECT_NAME='ZXing Android'
 export PROJECT_REPOSITORY='https://github.com/zxing/zxing.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
-export PROJECT_BUILD_ROOT='android'
+export PROJECT_BUILD_COMMAND='mvn -B -ntp -Pbuild-android package'
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec bash "$repository_root/tools/foundations/build_project.sh"
