7871da3bfd4398fc8656e97cf339405f4b6231aa
someXXYY <betaXXX@gmail.com>
2026-09-23 22:58:23 +0800

    fix metabase build entry point

--- java/metabase/linux/latest/build.sh
+++ java/metabase/linux/latest/build.sh
@@ -4,7 +4,7 @@ export PROJECT_NAME='Metabase'
 export PROJECT_REPOSITORY='https://github.com/metabase/metabase.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
-export PROJECT_BUILD_COMMAND='./bin/build uberjar'
+export PROJECT_BUILD_COMMAND='./bin/build.sh'
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec bash "$repository_root/tools/foundations/build_project.sh"
