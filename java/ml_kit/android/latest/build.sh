5d6dcc89ca5b097a8a6ccf3502380596bf4a4048
someXXYY <betaXXX@gmail.com>
2026-09-23 22:58:52 +0800

    build ml kit vision quickstart

--- java/ml_kit/android/latest/build.sh
+++ java/ml_kit/android/latest/build.sh
@@ -4,7 +4,7 @@ export PROJECT_NAME='Google ML Kit Android samples'
 export PROJECT_REPOSITORY='https://github.com/googlesamples/mlkit.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
-export PROJECT_BUILD_ROOT='android'
+export PROJECT_BUILD_ROOT='android/vision-quickstart'
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec bash "$repository_root/tools/foundations/build_project.sh"
