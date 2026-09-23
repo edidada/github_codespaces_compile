a1f0572ad15d53dced7f185c68e1592efc4ddd33
someXXYY <betaXXX@gmail.com>
2026-09-23 22:47:38 +0800

    fix metabase foundation script invocation

--- java/metabase/linux/latest/build.sh
+++ java/metabase/linux/latest/build.sh
@@ -7,4 +7,4 @@ export PROJECT_REF=''
 export PROJECT_BUILD_COMMAND='./bin/build uberjar'
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
-exec "$repository_root/tools/foundations/build_project.sh"
+exec bash "$repository_root/tools/foundations/build_project.sh"
