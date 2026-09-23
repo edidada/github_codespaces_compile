29e052edfcc6ae9dc793be1e55ca43a3aefd9242
someXXYY <betaXXX@gmail.com>
2026-09-23 22:34:25 +0800

    add metabase linux latest build

---
+++ java/metabase/linux/latest/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+export PROJECT_NAME='Metabase'
+export PROJECT_REPOSITORY='https://github.com/metabase/metabase.git'
+export PROJECT_VERSION='latest'
+export PROJECT_REF=''
+export PROJECT_BUILD_COMMAND='./bin/build uberjar'
+
+repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
+exec "$repository_root/tools/foundations/build_project.sh"
