181833d6962dfbbc3a4aa43f502b187a0f98ac90
somessyy <yyoiisdfasd@outlook.com>
2026-09-24 21:37:29 +0800

    fix(zxing): disable strict Javadoc doclint

--- java/zxing/android/latest/build.sh
+++ java/zxing/android/latest/build.sh
@@ -4,7 +4,7 @@ export PROJECT_NAME='ZXing Android'
 export PROJECT_REPOSITORY='https://github.com/zxing/zxing.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
-export PROJECT_BUILD_COMMAND='mvn -B -ntp -Pbuild-android package'
+export PROJECT_BUILD_COMMAND='mvn -B -ntp -Pbuild-android -DadditionalJOption=-Xdoclint:none package'
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec bash "$repository_root/tools/foundations/build_project.sh"
