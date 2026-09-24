76dee24b50f438b03575f8d588b78163adbe3681
somessyy <yyoiisdfasd@outlook.com>
2026-09-24 23:34:14 +0800

    fix(ml-kit): mark upstream Gradle wrapper executable

--- java/ml_kit/android/latest/build.sh
+++ java/ml_kit/android/latest/build.sh
@@ -5,7 +5,7 @@ export PROJECT_REPOSITORY='https://github.com/googlesamples/mlkit.git'
 export PROJECT_VERSION='latest'
 export PROJECT_REF=''
 export PROJECT_BUILD_ROOT='android/vision-quickstart'
-export PROJECT_BUILD_COMMAND="sed -i 's/ -XX:MaxPermSize=1024m//' gradle.properties && ./gradlew --no-daemon build"
+export PROJECT_BUILD_COMMAND="sed -i 's/ -XX:MaxPermSize=1024m//' gradle.properties && chmod +x gradlew && ./gradlew --no-daemon build"
 
 repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../.." && pwd)"
 exec bash "$repository_root/tools/foundations/build_project.sh"
